a8315fe7333ae806d4265505ea4de83e5ae53e8a
Esce-Tene <crescendo.phone@gmail.com>
2022-02-21 11:24:14 -0800

    Finished Project

---
+++ javascript.js
@@ -0,0 +1,50 @@
+const container = document.getElementById('container');
+const button = document.getElementById('reset');
+makeGrid(16);
+
+function makeGrid(n) {
+  for(let i = 0; i < n; i++) {
+    let row = document.createElement('div');
+    row.id = "row"
+    for(let j = 0; j < n; j++) {
+      let cell = document.createElement('div')
+      cell.style.height = (650 / n) + "px"
+      cell.style.width = (650 / n) + "px"
+      cell.id = "block"
+      cell.addEventListener('mouseover', hoverColor);
+      row.appendChild(cell);
+    document.getElementById("container").appendChild(row);
+  }
+  container.style.setProperty(
+    "grid-template-columns",
+    `repeat(${n}, 2fr)`
+  )}
+  container.style.setProperty(
+    "grid-template-row",
+    `repeat(${n}, 2fr)`
+  )}
+
+function hoverColor($event) {
+  const item = $event.target;
+  item.classList.add('white');
+}
+
+function promptPlayer() {
+  prompt("Enter a grid size")
+    if (prompt > 100) {
+      alert("No")
+    }
+}
+
+button.addEventListener('click', () => {
+  container.textContent = "";
+  let promptPlayer = prompt("Enter a grid size")
+  let gridSize = parseInt(promptPlayer);
+  if (gridSize > 100) {
+      alert("Max reached - Creating a 100x100 grid")
+      makeGrid(100);
+    }
+  else {
+    makeGrid(gridSize);
+  }
+})
